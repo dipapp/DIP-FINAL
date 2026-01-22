6b9f426bf9d2cab9dc8703fb06d768ae6eff64d3
dipapp <admin@dipmembers.com>
2026-01-22 12:14:53 -0800

    Add files via upload

--- app/dashboard/marketplace/page.tsx
+++ app/dashboard/marketplace/page.tsx
@@ -1443,7 +1443,7 @@ function ListingDetailModal({
           {/* Photos */}
           {listing.photoURLs.length > 0 ? (
             <div className="relative">
-              <div className="aspect-video bg-gray-100">
+              <div className="bg-gray-100 flex items-center justify-center" style={{ minHeight: '400px', maxHeight: '600px' }}>
                 <img
                   src={listing.photoURLs[currentPhotoIndex]}
                   alt={listing.title}
